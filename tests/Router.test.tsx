a546a09f56a5d1a0b51611e12ad94e3da93cc855
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-01-16 07:41:19 +0200

    16.01.2025

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -1,26 +1,39 @@
-import { render, screen } from "@testing-library/react";
-import { createMemoryRouter, RouterProvider } from "react-router-dom";
-import routes from "../src/routes";
+import { screen } from "@testing-library/react";
+import { db } from "./mocks/db";
+import { navigateTo } from "./utils";
 
 describe("Router", () => {
-
   it("should render the home page for /", () => {
+    navigateTo("/");
 
-    const router = createMemoryRouter(routes, {
-        initialEntries: ["/"]
-      });
-    render(<RouterProvider router={router} />);
-
-    expect(screen.getByRole("heading" , { name: /home/i })).toBeInTheDocument();
+    expect(screen.getByRole("heading", { name: /home/i })).toBeInTheDocument();
   });
 
   it("should render the products page for /products", () => {
+    navigateTo("/products");
 
-    const router = createMemoryRouter(routes, {
-        initialEntries: ["/products"]
-      });
-    render(<RouterProvider router={router} />);
+    expect(
+      screen.getByRole("heading", { name: /products/i })
+    ).toBeInTheDocument();
+  });
 
-    expect(screen.getByRole("heading" , { name: /products/i })).toBeInTheDocument();
+  it("should render the product datails page for /products/:id", async () => {
+    const product = db.product.create();
+
+    navigateTo("/products/" + product.id);
+
+    expect(
+      await screen.findByRole("heading", { name: product.name })
+    ).toBeInTheDocument();
+
+    db.product.delete({ where: { id: { equals: product.id } } });
+  });
+
+  it("should render the admin page for /admin",  () => {
+    navigateTo("/admin");
+
+    expect(
+     screen.getByRole("heading", { name: /admin/i })
+    ).toBeInTheDocument();
   });
 });
--- tests/setup.ts
+++ tests/setup.ts
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom/vitest";
 import ResizeObserver from "resize-observer-polyfill";
 import { server } from "./mocks/server";
 import { after } from "node:test";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, ReactNode } from "react";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
@@ -15,7 +15,8 @@ vi.mock('@auth0/auth0-react', () => ({
     user: undefined
   }),
   Auth0Provider: ({ children }: PropsWithChildren) => children,
-  withAuthenticationRequired: vi.fn()
+  // withAuthenticationRequired: vi.fn()
+  withAuthenticationRequired: (component: ReactNode) => component
 }));
 
 global.ResizeObserver = ResizeObserver;
